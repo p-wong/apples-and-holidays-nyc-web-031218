941a82ca797195814054ee63f8fe54a8097555a6
Patricia Wong <patriciawong08@gmail.com>
2018-02-26 16:35:09 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -67,7 +67,7 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.each do |seasons, holidays|
     holidays.map do |kholidays, vsupplies|
       kholidays if vsupplies.include?("BBQ")
-    end 
+    end.flatten.compact
   end 
 end
 
