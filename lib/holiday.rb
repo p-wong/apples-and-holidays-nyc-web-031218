1b709b58613038adda51c3a49f841abe6055b7e5
Patricia Wong <patriciawong08@gmail.com>
2018-02-26 16:46:11 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -59,7 +59,9 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |season, holidays|
-    puts ""
+    puts "#{season.capitalize}"
+    holidays.each do |holiday, supply|
+      "#{holiday}.split.capitalize!.join(): #{supply}.split.capitalize!.join()"
 end
 
 def all_holidays_with_bbq(holiday_hash)
